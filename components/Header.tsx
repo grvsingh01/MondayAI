1cdce306d35d63982068b1eb457fa4cce480e161
Gaurav Singh <grvsingh0101@gmail.com>
2023-06-20 16:42:33 +0530

    nun

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,15 +5,15 @@ export default function Header() {
   return (
     <header className="flex flex-col xs:flex-row justify-between items-center w-full mt-3 border-b pb-7 sm:px-4 px-2 border-gray-500 gap-2">
       <Link href="/" className="flex space-x-2">
-        {/* <Image
+        <Image
           alt="header text"
-          src="/bed.svg"
+          src="/vercelLogo.png"
           className="sm:w-10 sm:h-10 w-9 h-9"
           width={24}
           height={24}
-        /> */}
+        />
         <h1 className="sm:text-3xl text-xl font-bold ml-2 tracking-tight">
-          Texima.ai
+          Texima
         </h1>
       </Link>
       <a
